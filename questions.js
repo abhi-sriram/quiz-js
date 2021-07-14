383bdf5ad58c85596f05c489752183934f1d6e30
abhisriram <65218890+abhi-sriram@users.noreply.github.com>
2021-07-14 14:18:08 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,144 @@
+let questions = [
+  'Things you find in a classroom',
+    'Names of Desserts',
+    'Things you find at seaside',
+    'Names of clothing brands',
+    'Things that are blue in color',
+    'Names of cities in USA',
+]
+
+// let questions = [
+//     {
+//     numb: 1,
+//     question: "What does FTP stand for?",
+    
+    
+//   },
+//     {
+//     numb: 2,
+//     question: "Which of the following COVID-19 vaccine is a single dose vaccine?",
+//     answer: "Sputnik light",
+//     options: [
+//       "Moderna",
+//       "Covaxin",
+//       "Pfizer",
+//       "Sputnik light"
+//     ]
+//   },
+//   {
+//   numb: 3,
+//     question: "When FTP was established ?",
+//     answer: "Feb-19 2019",
+//     options: [
+//       "Feb-19 2019",
+//       "Mar-19 2019",
+//       "Dec-18 2018",
+//       "Jan-28 2019"
+//     ]
+//   },
+//     {
+//     numb: 4,
+//     question: "Which telecom company has become the first one in India to demonstrate the operation of the 5G network?",
+//     answer: "Airtel",
+//     options: [
+//       "Reliance Jio",
+//       "Airtel",
+//       "Vi",
+//       "Bsnl"
+//     ]
+//   },
+//     {
+//     numb: 5,
+//     question: "Who has been recommended by the BCCI for the Rajiv Gandhi Khel Ratna Award 2021?",
+//     answer: "Ravichandran Ashwin",
+//     options: [
+//       "KL Rahul",
+//       "Jasprit Bumrah",
+//       "Ravichandran Ashwin",
+//       "Shikar Dhawan"
+//     ]
+//   },
+//     {
+//     numb: 6,
+//     question: "A person crosses a 600 m long street in 5 minutes. What is his speed in km per hour?",
+//     answer: "7.2",
+//     options: [
+//       "3.6",
+//       "10",
+//       "8.4",
+//       "7.2"
+//     ]
+//   },
+//   {
+//   numb: 7,
+//     question: "Who is FTP's Chief Communications Officer?",
+//     answer: "Yuraj Kanni",
+//     options: [
+//       "Uday Dumma",
+//       "Yuraj Kanni",
+//       "Gajam Jyosthna",
+//       "Prashanth"
+//     ]
+//   },
+//   {
+//   numb: 8,
+//     question: "Entomology is the science that studies___________.",
+//     answer: "Insects",
+//     options: [
+//       "Behavior of human beings",
+//       "Insects",
+//       "The origin and history of technical and scientific terms",
+//       "The formation of rocks"
+//     ]
+//   },
+//   {
+//   numb: 9,
+//     question: "Who is the English physicist responsible for the 'Big Bang Theory'?",
+//     answer: "George Gamow",
+//     options: [
+//       "Albert Einstein",
+//       "IMichael Skube",
+//       "George Gamow",
+//       "Roger Penrose"
+//     ]
+//   },
+//   {
+//   numb: 10,
+//     question: "The Centre for Cellular and Molecular Biology is situated at?",
+//     answer: "Hyderabad",
+//     options: [
+//       "Mumbai",
+//       "Banglore",
+//       "Hyderabad",
+//       "Chennai"
+//     ]
+//   },
+  
+//   {
+//   numb: 11,
+//     question: "How was the Interview ?",
+//     answer: "All are Correcct",
+//     options: [
+//       "Good",
+//       "Excellent",
+//       "Fabulous",
+//       "No words",
+//       "All are Correct"
+
+//     ]
+//   },
+  
+//   // you can uncomment the below codes and make duplicate as more as you want to add question
+//   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+//   //   {
+//   //   numb: 6,
+//   //   question: "Your Question is Here",
+//   //   answer: "Correct answer of the question is here",
+//   //   options: [
+//   //     "Option 1",
+//   //     "option 2",
+//   //     "option 3",
+//   //     "option 4"
+//   //   ]
+//   // },
+// ];
